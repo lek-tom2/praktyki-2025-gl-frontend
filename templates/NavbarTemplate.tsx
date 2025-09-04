565829988017df5f2d394f94461d8e568ea82649
Tomek <lekstomek602@gmail.com>
2025-09-04 14:38:43 +0200

    navbar template complated, colors look strange due to incorrect colors in global.css for this style

--- templates/NavbarTemplate.tsx
+++ templates/NavbarTemplate.tsx
@@ -1,34 +1,68 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-// based on users context display diffrent stuff
-
 const NavbarTemplate = () => {
-  return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+  // logged user
+  if (false) {
+    return (
+      <nav className="bg-primary h-12 w-full flex flex-row items-center justify-start text-neutral px-6">
+        {/* Logo */}
+        <div className="relative w-20 h-6 ml-20">
+          <Image src={"/logo.png"} fill alt="GlobalPark" />
+        </div>
+
+        {/* Center links */}
+        <ul className="flex flex-row gap-6 flex-1 justify-center">
           <li>
-            <a>Link</a>
+            <Link href={"#"} className="hover:underline">
+              Find Parking
+            </Link>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
+            <Link href={"#"} className="hover:underline">
+              Contact
+            </Link>
           </li>
         </ul>
+
+        <div className="rounded-4xl">
+          <label className="input flex items-center gap-2 h-8 rounded-4xl">
+            <input type="text" placeholder="Search..." className="grow" />
+          </label>
+        </div>
+        <div className="mr-20">
+          <div className="avatar relative w-8 h-8 bg-gray-300 rounded-full ml-6">
+            <Image src={"/defaultUser.png"} fill={true} alt={"G"}></Image>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+
+  // unlogged user
+  return (
+    <nav className="bg-primary h-12 w-full flex flex-row items-center justify-start text-neutral px-6">
+      {/* Logo */}
+      <div className="relative w-20 h-6 ml-30 mr-auto">
+        <Image src={"/logo.png"} fill alt="GlobalPark" />
       </div>
-    </div>
+
+      <div className="flex flex-row items-center justify-between w-[10vw] mr-30 ml-auto ">
+        <Link
+          href="/auth/login"
+          className="btn rounded-4xl w-[50%] text-center h-8 bg-accent ml-[-1rem] min-w-6"
+        >
+          Login
+        </Link>
+        <Link
+          href="/auth/register"
+          className="btn rounded-4xl w-[50%] text-center h-8 bg-accent min-w-6 "
+        >
+          Register
+        </Link>
+      </div>
+    </nav>
   );
 };
 
