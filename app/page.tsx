815c31b3eff6e045b9bc530c8a734988ce24ee49
Tomek <lekstomek602@gmail.com>
2025-09-10 13:13:59 +0200

    added gl brand indentity themes
    fixed color problems on footer navbar welcome login register

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use server";
+import Welcome from "@/pages/Welcome";
 import PageTemplate from "@/templates/PageTemplate";
 
 export default async function Home() {
-  return <Welcome></Welcome>
+  return <Welcome></Welcome>;
 }
