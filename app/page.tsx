f90cd9a28e61f158c7c7ba95195f4cd6af1152d4
Nikodem <nikodem2287@gmail.com>
2025-09-16 12:11:34 +0200

    Waiting for other components to create links and home page.

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,6 @@ import PageTemplate from "@/templates/PageTemplate";
 import Welcome from "@/pages/Welcome";
 
 
-export default async function Home() {
-  // return <Welcome></Welcome>
-  // return (
-  //   <PageTemplate>
-  //     {/* <ParkingSpotList parkingSpots={spotsPL2} level="PL2" /> */}
-  //     <ParkingSpotList parkingSpots={spotsPL3} level="PL3" />
-  //   </PageTemplate>
-  // );
-  return (
-    <PageTemplate>
-      <ParkingSpotMap parkingSpots={spotsPL2} level="PL2" />
-      {/* <ParkingSpotMap parkingSpots={spotsPL3} level="PL3" /> */}
-    </PageTemplate>
-  );
+export default async function Start() {
+    return <Welcome></Welcome>
 }
