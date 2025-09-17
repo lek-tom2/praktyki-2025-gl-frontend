55169fb0c8b281a4a02888b02ed914d024d8da92
Nikodem <nikodem2287@gmail.com>
2025-09-17 11:59:26 +0200

    Finished css on login/signup

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,6 @@ import ParkingSpotMap from "@/components/parkingSpotMap/ParkingSpotMap";
 import ParkingManager from "@/components/parkingManager/parkingManager";
 
 export default async function Home() {
-  // return <Welcome></Welcome>
-  // return (
-  //   <PageTemplate>
-  //     {/* <ParkingSpotList parkingSpots={spotsPL2} level="PL2" /> */}
-  //     <ParkingSpotList parkingSpots={spotsPL3} level="PL3" />
-  //   </PageTemplate>
-  // );
-  return (
-    <PageTemplate>
-      {/* <ParkingSpotMap parkingSpots={spotsPL2} level="PL2" /> */}
-      {/* <ParkingSpotMap parkingSpots={spotsPL3} level="PL3" /> */}
-      <ParkingManager />
-    </PageTemplate>
-  );
+  return <Welcome></Welcome>
+  
 }
