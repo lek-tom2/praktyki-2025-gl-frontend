386ba03e489046760e1b064ab62b555b1a97ac0f
Kasper Kawiecki <kasper.kawiecki@globallogic.com>
2025-09-19 11:55:12 +0200

    Configuring Next.js to ignore ESlint errors during production build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Add this block to disable build-time linting
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
